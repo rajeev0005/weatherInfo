9872fe4b77a366fd1c10c4bdff3267f839c1105d
Rajeev singh <rs5375558@gmail.com>
2024-07-30 19:53:31 +0530

    first commit

--- src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "19f53356ac018d4536ec6881a89b0ef6",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
